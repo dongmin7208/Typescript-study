74112461074e98f8e5641ad8b5676d41e63711c4
dongmin7208 <dongmin.park.career@gmail.com>
2024-03-24 01:00:32 +0900

    type basic

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,44 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 123.123;
+let num4: number = 123e3;
+let num5: number = 123e-3;
+let num6: number = 0x123;
+let num7: number = 0o123;
+let num8: number = 0b1010;
+let num9: number = Infinity;
+let num10: number = NaN;
+let num11: number = parseInt("123");
+let num12: number = parseFloat("123.123");
+let num13: number = Number("123");
+let num14: number = Number("123.123");
+
+// string
+let str1: string = "hello";
+let str2: string = "hello";
+let str3: string = `hello`;
+let str4: string = "hello" + "world";
+let str5: string = `hello ${str4}`;
+
+//boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+let bool3: boolean = Boolean(1);
+let bool4: boolean = Boolean(0);
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+// let numA: number = null;
+
+//  리터럴 -> 값 리터럴 타입
+let numA: 10 = 10;
+let strA: "hello" = "hello";
+// let boolA: true = false;
+let boolA: false = false;
+let boolB: true = true;
+// numA = 12;
