37aafed50f4f579ff5270974b00cfcb4fec8a140
dongmin7208 <dongmin.park.career@gmail.com>
2024-03-24 00:13:33 +0900

    module:CommonJS

--- section1/src/index.js
+++ section1/src/index.js
@@ -1,2 +1,4 @@
-console.log("hello typescript");
-var a = 1;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const hello_1 = require("./hello");
+(0, hello_1.hello)();
--- section1/src/index.ts
+++ section1/src/index.ts
@@ -1,2 +1,3 @@
-console.log("hello typescript");
-const a: number = 1;
+import { hello } from "./hello";
+
+hello();
