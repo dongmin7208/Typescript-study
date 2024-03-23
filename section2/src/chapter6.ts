37e0a9ff3a967d112f826dca63386fd4af57d66f
dongmin7208 <dongmin.park.career@gmail.com>
2024-03-24 01:37:36 +0900

    any , unknown

---
+++ section2/src/chapter6.ts
@@ -0,0 +1,20 @@
+// // any
+
+// let any1: any = 123;
+// let any2: any = "123";
+// let any3: any = true;
+
+// any1 = false;
+
+// unknown
+let unknownVar: unknown;
+
+unknownVar = "";
+unknownVar = 123;
+unknownVar = () => {};
+
+// unknownVar.toUpperCase();
+
+if (typeof unknownVar === "string") {
+  unknownVar.toUpperCase();
+}
